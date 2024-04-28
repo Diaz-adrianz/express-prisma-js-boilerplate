1847e99e957d63b413db927a76b2c9919ee7871b
Diaz-adrianz <diazadrianz191205@gmail.com>
2024-04-28 12:23:59 +0700

    chore(db): user and seeder
    Signed-off-by: Diaz-adrianz <diazadrianz191205@gmail.com>

--- prisma/seeder/index.js
+++ prisma/seeder/index.js
@@ -1,24 +1,32 @@
 import { PrismaClient } from '@prisma/client';
 import * as fs from 'fs/promises';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
+const path = 'prisma/seeder/data';
 
 async function seedDatabase() {
 	let files = [];
 
 	try {
-		files = await fs.readdir('./data');
+		files = await fs.readdir(path);
 	} catch (error) {
 		console.error('ERR(read seeder data): ', error);
 		return;
 	}
 
 	try {
-		for (let file in files) {
-			const jsonFile = await fs.readFile('./data/' + file, 'utf8');
+		for (let file of files) {
+			const jsonFile = await fs.readFile(path + '/' + file, 'utf8');
 			const data = JSON.parse(jsonFile);
 
 			for (const dat of data) {
+				const keys = Object.keys(dat);
+				if (keys.includes('password')) {
+					const salt = await bcrypt.genSalt();
+					dat['password'] = await bcrypt.hash(dat['password'], salt);
+				}
+
 				await prisma[file.split('.')[0]].create({
 					data: dat,
 				});
